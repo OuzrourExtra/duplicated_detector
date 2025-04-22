8041772ce0764b29b074dcba5111e50e2929ffc9
Ilyas Ouzrour <ilyas.ouzrour@gmail.com>
2025-04-22 23:14:07 +0100

    changing the core

--- main.go
+++ main.go
@@ -1,24 +1,39 @@
-// duplicated_detector prints the text of each line that appaears more than
-// once in the standard input , preceded by its counts
+// duplicated_detector count the duplicated lines
 
 package main
 
-import(
+import (
 	"fmt"
 	"bufio"
 	"os"
 )
+func countLines(file *os.File , countsMap *map[string]int){
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan(){
+		(*countsMap)[scanner.Text()]++
+	}
+}
 
 func main(){
-	counts := make(map[string]int)
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan(){
-		counts[scanner.Text()]++
+	countsMap := make(map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin,&countsMap)
+	} else{
+		for _ , file := range files{
+			f , err := os.Open(file)
+			if err!=nil{
+				fmt.Fprintf(os.Stderr,"Error : %v",err)
+				continue
+			}
+			countLines(f,&countsMap)
+			f.Close()
+		}
 	}
 
-	for line , n := range counts{
-		if n>1{
-			fmt.Printf("%d\t%s\n",n,line)
+	for line , occurence := range countsMap{
+		if occurence > 1 {
+			fmt.Printf("%s \t %d \n",line,occurence)
 		}
 	}
 }
